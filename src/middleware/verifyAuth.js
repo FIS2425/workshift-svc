f0aaa5d1e7ac3de68ff9dfcb5fd303bf8128c743
Alvaro Jesus Bernal Caunedo <80347035+alvarobernal2412@users.noreply.github.com>
2024-11-25 10:47:11 +0100

    refactor(controller): ensured req.headers is defined (#28)
    * refactor(controller): ensured req.headers is defined
    
    * fix: updated deprecated kafka-node
    
    * chore(ci): added unstable publishing
    
    ---------
    
    Co-authored-by: antonio <antoniorodriguezruiz2002@gmail.com>

--- src/middleware/verifyAuth.js
+++ src/middleware/verifyAuth.js
@@ -9,7 +9,7 @@ export const verifyAuth = (req, res, next) => {
     logger.error('Error on token validation', {
       method: req.method,
       url: req.originalUrl,
-      ip: req.headers['x-forwarded-for'] || req.ip,
+      ip: req.headers && req.headers['x-forwarded-for'] || req.ip,
       requestId: req.headers && req.headers['x-request-id'] || null,
       error: 'Access denied: No token provided',
     });
@@ -22,7 +22,7 @@ export const verifyAuth = (req, res, next) => {
       logger.error('Error on token validation', {
         method: req.method,
         url: req.originalUrl,
-        ip: req.headers['x-forwarded-for'] || req.ip,
+        ip: req.headers && req.headers['x-forwarded-for'] || req.ip,
         requestId: req.headers && req.headers['x-request-id'] || null,
         userId: decoded.userId,
         error: 'Access denied. Insufficient permissions.',
@@ -36,7 +36,7 @@ export const verifyAuth = (req, res, next) => {
     logger.error('Error on token validation', {
       method: req.method,
       url: req.originalUrl,
-      ip: req.headers['x-forwarded-for'] || req.ip,
+      ip: req.headers && req.headers['x-forwarded-for'] || req.ip,
       requestId: req.headers && req.headers['x-request-id'] || null,
       error: error.message
     });
--- src/utils/kafkaTransport.js
+++ src/utils/kafkaTransport.js
@@ -1,34 +1,35 @@
 import winston from 'winston';
-import { KafkaClient, Producer } from 'kafka-node';
+import { Kafka } from 'kafkajs';
 
 class KafkaTransport extends winston.Transport {
   constructor(opts) {
     super(opts);
 
-    this.client = new KafkaClient({ kafkaHost: opts.kafkaHost || process.env.KAFKA_HOST });
-    this.producer = new Producer(this.client);
+    this.client = new Kafka({
+      clientId: 'auth',
+      brokers: [process.env.KAFKA_HOST]
+    });
+    this.producer = this.client.producer();
 
     this.topic = opts.topic || 'logs';
-    this.producer.on('ready', () => console.log('Kafka producer is ready'));
-    this.producer.on('error', (err) => console.error('Kafka producer error:', err));
   }
 
-  log(info, callback) {
-    const message = JSON.stringify({
-      ...info
-    });
+  async log(info, callback) {
+    const message = {
+      value: JSON.stringify({ ...info })
+    };
 
-    const payloads = [{ topic: this.topic, messages: message }];
+    const payloads = { topic: this.topic, messages: [message] };
 
     // Send the log to Kafka
-    this.producer.send(payloads, (err) => {
-      if (err) {
-        console.error('Error sending log to Kafka:', err);
-      }
-
-      if (callback) callback();
-    });
+    try {
+      await this.producer.connect();
+      await this.producer.send(payloads);
+      await this.producer.disconnect();
+    } catch (error) {
+      callback(error, false);
+    }
   }
-}
+};
 
 export default KafkaTransport;
